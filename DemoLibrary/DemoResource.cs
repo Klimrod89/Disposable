0a667605352adf253801997b678150be4615387c
Ymac VSCode <51804657+Klimrod89@users.noreply.github.com>
2024-02-06 17:21:11 -0500

    Completed assignment until advanced learning

---
+++ DemoLibrary/DemoResource.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoLibrary;
+
+public class DemoResource : IDisposable
+{
+    public void Dispose()
+    {
+        Console.WriteLine("Closing Connection via Dispose");
+    }
+
+    public void DoWork()
+    {
+        Console.WriteLine("Opening Connection");
+        Console.WriteLine("Doing work...");
+        throw new Exception("I broke");
+        Console.WriteLine("Closing Connection");
+    }  
+}
